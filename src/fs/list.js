576c110ea6612e3fb96393964a2ccc500ed72633
jazzzrabbit <jazzzrabbit@gmail.com>
2022-12-01 20:55:43 +0300

    feat: add list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,20 @@
+import fs from 'fs/promises';
+import * as url from 'url';
+import path from 'path';
+
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+
 const list = async () => {
-    // Write your code here 
+  const files = `files`;
+  const errorMsg = 'FS operation failed';
+  let dir;
+
+  try {
+    dir = await fs.readdir(path.join(__dirname, files));
+    console.table(dir);
+  } catch {
+    throw new Error(errorMsg);
+  }
 };
 
 await list();
