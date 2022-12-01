35310585d0287d62724b80a2a3f617218e4210c5
jazzzrabbit <jazzzrabbit@gmail.com>
2022-12-01 21:54:27 +0300

    feat: add env

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,9 @@
 const parseEnv = () => {
-    // Write your code here 
+  const env = process.env;
+  const regex = /rss_/gi;
+  Object.keys(env).forEach(key => {
+    if (key.match(regex)) console.log(key + '=' + env[key]);
+  });
 };
 
 parseEnv();
