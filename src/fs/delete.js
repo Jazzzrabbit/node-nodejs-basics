756bccb6a1d6d34bcdfe800cebafec5b0ee77a19
jazzzrabbit <jazzzrabbit@gmail.com>
2022-12-01 18:24:58 +0300

    feat: add delete

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,25 @@
+import fs from 'fs/promises';
+import * as url from 'url';
+import path from 'path';
+
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+
 const remove = async () => {
-    // Write your code here 
+  const remove = 'fileToRemove.txt';
+  const files = `files`;
+  const errorMsg = 'FS operation failed';
+  let dir;
+
+  try {
+    dir = await fs.readdir(path.join(__dirname, files));
+  } catch {
+    throw new Error(errorMsg);
+  }
+
+  if (!dir.includes(remove)) throw new Error(errorMsg);
+  else {
+    fs.rm(path.join(__dirname, files, remove));
+  }
 };
 
 await remove();
