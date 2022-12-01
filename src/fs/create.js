cd9b7761d044b258bffe8e841a8dba0122eba156
jazzzrabbit <jazzzrabbit@gmail.com>
2022-12-01 14:24:27 +0300

    feat: add create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,11 @@
+import fs from 'fs/promises';
+
 const create = async () => {
-    // Write your code here 
+  const path = './files';
+  const file = 'fresh.txt';
+  const dir = await fs.readdir(path);
+  if (dir.includes(file)) throw new Error('FS operation failed');
+  else fs.writeFile('./files/fresh.txt', 'I am fresh and young');
 };
 
 await create();
