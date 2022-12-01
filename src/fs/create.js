0648d0a2ff6d6f5207a5327f1021da9fd9af962b
jazzzrabbit <jazzzrabbit@gmail.com>
2022-12-01 17:58:46 +0300

    feat: add copy

--- src/fs/create.js
+++ src/fs/create.js
@@ -3,9 +3,11 @@ import fs from 'fs/promises';
 const create = async () => {
   const path = './files';
   const file = 'fresh.txt';
+  const msg = 'I am fresh and young';
+  const errorMsg = 'FS operation failed';
   const dir = await fs.readdir(path);
-  if (dir.includes(file)) throw new Error('FS operation failed');
-  else fs.writeFile('./files/fresh.txt', 'I am fresh and young');
+  if (dir.includes(file)) throw new Error(errorMsg);
+  else fs.writeFile(`${path}/${file}`, msg);
 };
 
 await create();
