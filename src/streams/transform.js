d45b8d33b384de51efdb26176bc91372364cec6b
jazzzrabbit <jazzzrabbit@gmail.com>
2022-12-02 19:47:44 +0300

    feat: add transform

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,24 @@
+import { Transform, pipeline } from 'stream';
+
 const transform = async () => {
-    // Write your code here 
+  try {
+    console.log('Type something. It will be reverted. Press ctrl+c to exit.')
+    const read = process.stdin;
+    const write = process.stdout;
+
+    const reverse = new Transform({
+      transform(chunk, encode, callback) {
+        const string = chunk.toString().trim();
+        const transformed = string.split('').reverse().join('');
+        this.push(`${transformed}\n`);
+        callback();
+      }
+    });
+
+    pipeline(read, reverse, write, err => console.error(err));
+  } catch(e) {
+    console.error(e);
+  }
 };
 
 await transform();
