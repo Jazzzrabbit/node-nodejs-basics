235901ed3b6fc68aa3399ca6265f9ac73c138c9d
jazzzrabbit <jazzzrabbit@gmail.com>
2022-12-02 14:06:03 +0300

    feat: add read stream

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,25 @@
+import fs from 'fs/promises';
+import path from 'path';
+import * as url from 'url';
+
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+
 const read = async () => {
-    // Write your code here 
+  const files = 'files';
+  const file = 'fileToRead.txt';
+
+  try {
+    const opened = await fs.open(path.join(__dirname, files, file));
+    const stream = opened.createReadStream();
+    stream
+      .on('data', chunk => process.stdout.write(chunk))
+      .on('end', () => {
+        process.stdout.write('\n');
+        stream.destroy();
+      });
+  } catch(e) {
+    console.error(e);
+  }
 };
 
 await read();
