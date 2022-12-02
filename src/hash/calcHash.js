c8b3b7cc8b5dde40ddc0dd96adbd6fd13ef815f5
jazzzrabbit <jazzzrabbit@gmail.com>
2022-12-02 13:15:21 +0300

    feat: add hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,22 @@
+import fs from 'fs/promises';
+import crypto from 'crypto';
+import path from 'path';
+import * as url from 'url';
+
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+
 const calculateHash = async () => {
-    // Write your code here 
+  const files = 'files';
+  const file = 'fileToCalculateHashFor.txt';
+
+  try {
+    const content = await fs.readFile(path.join(__dirname, files, file));
+    const hash = crypto.createHash('sha256').update(content);
+    const hex = hash.digest('hex');
+    console.log(hex);
+  } catch(e) {
+    console.error(e);
+  }
 };
 
 await calculateHash();
