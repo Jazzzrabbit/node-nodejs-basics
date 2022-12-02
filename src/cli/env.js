520bafd1885aa4705dfe73a8ae22daa8b2091392
jazzzrabbit <jazzzrabbit@gmail.com>
2022-12-02 09:02:51 +0300

    refactor: change env output

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,9 +1,11 @@
 const parseEnv = () => {
   const env = process.env;
   const regex = /rss_/gi;
+  const output = [];
   Object.keys(env).forEach(key => {
-    if (key.match(regex)) console.log(key + '=' + env[key]);
+    if (key.match(regex)) output.push(key + '=' + env[key]);
   });
+  console.log(output.join('; '));
 };
 
 parseEnv();
