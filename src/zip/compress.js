608947f381bcf18cb487d791001244cc94c99e5d
jazzzrabbit <jazzzrabbit@gmail.com>
2022-12-03 14:56:42 +0300

    feat: add compress

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,26 @@
+import fs from 'fs';
+import zlib from 'zlib';
+import { pipeline } from 'stream';
+import { promisify } from 'util';
+import path from 'path';
+import * as url from 'url';
+
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+
 const compress = async () => {
-    // Write your code here 
+  const files = 'files';
+  const file = 'fileToCompress.txt';
+  const compressed = 'archive.gz';
+  
+  try {
+    const readStream = fs.createReadStream(path.join(__dirname, files, file));
+    const writeStream = fs.createWriteStream(path.join(__dirname, files, compressed));
+    const compress = zlib.createGzip();
+    const asyncPipeline = promisify(pipeline);
+    await asyncPipeline(readStream, compress, writeStream);
+  } catch(e) {
+    console.error(e);
+  }
 };
 
 await compress();
