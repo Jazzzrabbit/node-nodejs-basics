2c1376efb693c27801e02762eb46780085ffc170
jazzzrabbit <jazzzrabbit@gmail.com>
2022-12-04 11:43:26 +0300

    feat: add worker threads

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,32 @@
+import { Worker } from 'worker_threads';
+import os from 'os';
+import path from 'path';
+import * as url from 'url';
+
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+
 const performCalculations = async () => {
-    // Write your code here
+  const file = 'worker.js';
+  const filename = path.join(__dirname, file);
+  const threads = os.cpus().length;
+  const workers = [];
+  
+  for (let i = 0; i < threads; i++) {
+    workers.push(new Promise((res, rej) => {
+      const worker = new Worker(filename, { workerData: 10 + i });
+      worker
+        .on('message', data => res({ 
+          'status': 'resolved',
+          'data': data 
+        }))
+        .on('error', () => rej({
+          'status': 'error',
+          'data': null
+        }));
+    }));
+  }
+
+  await Promise.all(workers).then(data => console.log(data));
 };
 
 await performCalculations();
