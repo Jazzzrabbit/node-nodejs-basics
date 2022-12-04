17165e6621bb035abd780b2610dd1fe6bbd93157
jazzzrabbit <jazzzrabbit@gmail.com>
2022-12-04 16:51:40 +0300

    feat: add child process

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,15 @@
+import { fork } from 'child_process';
+import path from 'path';
+import * as url from 'url';
+
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const files = 'files';
+  const file = 'script.js';
+  const fileName = path.join(__dirname, files, file);
+  const child = fork(fileName, args);
+  child.on('close', () => console.log('Don`t worry, be happy and sleep more!'));
 };
 
-spawnChildProcess();
+spawnChildProcess(['Type anything to get a response from master process.', 'Type CLOSE to exit.']);
