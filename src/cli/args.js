ba14e5bf4680a442b90ce9880aaba2ca29ad9076
jazzzrabbit <jazzzrabbit@gmail.com>
2022-12-04 17:36:02 +0300

    refactor: change output in args

--- src/cli/args.js
+++ src/cli/args.js
@@ -3,7 +3,7 @@ const parseArgs = () => {
   const regex = /--/gi;
   const output = [];
   args.map((arg, i) => {
-    if (arg.match(regex)) output.push(arg + ' is ' + args[i + 1]);
+    if (arg.match(regex)) output.push(arg.slice(2) + ' is ' + args[i + 1]);
   });
   console.log(output.join(', '));
 };
